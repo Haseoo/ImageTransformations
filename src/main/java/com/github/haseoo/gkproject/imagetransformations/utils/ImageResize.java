d43a7de8fbf0e406e3cfd8b6baef57973c6a72ae
Haseoo <dawid_szymkiewicz@hotmail.com>
2019-11-17 19:52:25 +0100

    spr

--- src/main/java/com/github/haseoo/gkproject/imagetransformations/utils/ImageResize.java
+++ src/main/java/com/github/haseoo/gkproject/imagetransformations/utils/ImageResize.java
@@ -1,14 +1,12 @@
 package com.github.haseoo.gkproject.imagetransformations.utils;
 
 import javafx.scene.image.Image;
-import javafx.scene.image.PixelReader;
 import javafx.scene.paint.Color;
 import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
 import lombok.val;
 
 import static com.github.haseoo.gkproject.imagetransformations.utils.JavaFXUtils.createImage;
-import static javafx.scene.paint.Color.color;
 
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class ImageResize {
@@ -24,14 +22,65 @@ public class ImageResize {
         Pair<Integer> newDimensions = calculateNewDimensions(sourceImage, scale.getWidth(), scale.getHeight());
         Pair<Integer> oldDimensions = new Pair<>((int) sourceImage.getWidth(), (int) sourceImage.getHeight());
         val source = sourceImage.getPixelReader();
-        Pair<Double> ratio = new Pair<>((oldDimensions.getWidth() * 1.0) / (newDimensions.getWidth() * 1.0),
-                (oldDimensions.getHeight() * 1.0) / (newDimensions.getHeight() * 1.0));
         return createImage(newDimensions, point -> {
-            Pair<Double> diff = new Pair<>(point.getX() * ratio.getX() - (int) (point.getX() * ratio.getX()),
-                    point.getY() * ratio.getY() - (int) (point.getY() * ratio.getY()));
-            Pair<Integer> first = new Pair<>((int) (point.getX() * ratio.getX()), (int) (point.getY() * ratio.getY()));
-            Pair<Integer> second = getSecondPoint(oldDimensions, first);
-            return interpolateColor(source, diff, first, second);
+            double ratioX = (oldDimensions.getWidth()*1.0)/(newDimensions.getWidth()*1.0);
+            double ratioY = (oldDimensions.getHeight()*1.0)/(newDimensions.getHeight()*1.0);
+            double x,y,dx,dy;
+
+            int firstX,firstY,secondX, secondY;
+
+            double red1,red2,red3,red4,red5,
+                    green1,green2,green3,green4,green5,
+                    blue1,blue2, blue3,blue4,blue5;
+
+            x = (point.getX()*ratioX);
+            y = (point.getY()*ratioY);
+            dx  = point.getX()*ratioX - (int)(point.getX()*ratioX);
+            dy = point.getY()*ratioY - (int)(point.getY()*ratioY);
+            firstX = (int)x;
+            firstY = (int)y;
+            if(firstX + 1 >= oldDimensions.getX()){
+                secondX = firstX;
+            }else{
+                secondX = firstX + 1;
+            }
+            if(firstY+1 >= oldDimensions.getY()){
+                secondY = firstY;
+            }else{
+                secondY = firstY+1;
+            }
+            if(source.getColor(firstX,firstY).getOpacity() == 0){
+                return Color.WHITE;
+            }
+//                kolory pixeli sasiadujacych gornych
+            red1 = source.getColor(firstX,firstY).getRed();
+            red2 = source.getColor(secondX,firstY).getRed();
+            green1 = source.getColor(firstX,firstY).getGreen();
+            green2 = source.getColor(secondX,firstY).getGreen();
+            blue1 = source.getColor(firstX,firstY).getBlue();
+            blue2 = source.getColor(secondX,firstY).getBlue();
+//                interpolacja pozioma dla gory
+            red3 = ((red1*(1-dx))+(red2*dx));
+            green3 = ((green1*(1-dx))+(green2*dx));
+            blue3 = ((blue1*(1-dx))+(blue2*dx));
+
+//                kolory pixeli sasiadujacych dolnych
+            red1 = source.getColor(firstX,secondY).getRed();
+            red2 = source.getColor(secondX,secondY).getRed();
+            green1 =  source.getColor(firstX,secondY).getGreen();
+            green2 =  source.getColor(secondX,secondY).getGreen();
+            blue1 =  source.getColor(firstX,secondY).getBlue();
+            blue2 =  source.getColor(secondX,secondY).getBlue();
+//                interpolacja pozioma dla dolu
+            red4 = ((red1*(1-dx))+(red2*dx));
+            green4 = ((green1*(1-dx))+(green2*dx));
+            blue4 = ((blue1*(1-dx))+(blue2*dx));
+
+//                interpolacja pionowa
+            red5 = ((red3*(1-dy))+(red4*dy));
+            green5 = ((green3*(1-dy))+(green4*dy));
+            blue5 = ((blue3*(1-dy))+(blue4*dy));
+            return Color.color(red5,green5,blue5);
         });
     }
 
@@ -43,31 +92,6 @@ public class ImageResize {
     private static Pair<Double> calculateNewScale(Pair<Double> oldDimensions, Pair<Integer> newDimensions) {
         return new Pair<>(oldDimensions.getWidth() / (double) newDimensions.getWidth(),
                 oldDimensions.getHeight() / (double) newDimensions.getHeight());
-    }
-
-    private static Color interpolateColor(PixelReader source, Pair<Double> diff, Pair<Integer> firstPoint, Pair<Integer> secondPoint) {
-        Pair<Color> upperColors = getColorPair(source, firstPoint, secondPoint, firstPoint.getY());
-        Pair<Color> lowerColors = getColorPair(source, firstPoint, secondPoint, secondPoint.getY());
-
-        Color interpolatedUpperColor = getEffectiveColor(diff, upperColors.getX(), upperColors.getY());
-        Color interpolatedLowerColor = getEffectiveColor(diff, lowerColors.getX(), lowerColors.getY());
-
-        return getEffectiveColor(diff, interpolatedUpperColor, interpolatedLowerColor);
-    }
-
-    private static Pair<Color> getColorPair(PixelReader source, Pair<Integer> firstPoint, Pair<Integer> secondPoint, Integer y) {
-        return new Pair<>(color((source.getColor(firstPoint.getX(), y).getRed()),
-                source.getColor(firstPoint.getX(), y).getGreen(),
-                source.getColor(firstPoint.getX(), y).getBlue()),
-                color(source.getColor(secondPoint.getX(), y).getRed(),
-                        source.getColor(secondPoint.getX(), y).getGreen(),
-                        source.getColor(secondPoint.getX(), y).getBlue()));
-    }
-
-    private static Color getEffectiveColor(Pair<Double> diff, Color color1, Color color2) {
-        return color((color1.getRed() * (1 - diff.getX())) + (color2.getRed() * diff.getX()),
-                (color1.getGreen() * (1 - diff.getX())) + (color2.getGreen() * diff.getX()),
-                (color1.getBlue() * (1 - diff.getX())) + (color2.getBlue() * diff.getX()));
     }
 
     private static Pair<Integer> getSecondPoint(Pair<Integer> oldDimensions, Pair<Integer> first) {
